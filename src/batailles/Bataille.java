0a9c6e12a4d5d5af48d8335d92fba7a64610d818
BELGNAOUI Othman <belgnaoui.othman@gmail.com>
2024-02-28 12:55:15 +0100

    Création de l'interface Bataille

--- src/batailles/Bataille.java
+++ src/batailles/Bataille.java
@@ -1,18 +1,24 @@
 package batailles;
 
-import java.util.List;
-
-public class Bataille {
-    private List<Combattant> combattants;
-    private Lieu lieu;
-
-    public Bataille(List<Combattant> combattants, Lieu lieu) {
-        this.combattants = combattants;
-        this.lieu = lieu;
-    }
-
-    public void demarrer() {
-        // Logique de d�marrage de la bataille
-        
-    }
+public interface Bataille {
+	
+	private void decrireContext() {
+		System.out.print("Décriscription du context :");
+	}
+	
+	private void choisirPersonnages() {
+		System.out.print("Choisissez vos personnages :");
+	}
+	
+	private void preparerLaBataille() {
+		System.out.print("Préparation de la bataille :");
+	}
+	
+	private void decrireLaBataille() {
+		System.out.print("Description de la bataille :");
+	}
+	
+	private void resultatBataille() {
+		System.out.print("Préparation de la bataille :");
+	}
 }
